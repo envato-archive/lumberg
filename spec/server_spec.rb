fe079a357b31ad69ff5783bf3d350169bf667543
Justin Mazzi <jmazzi@gmail.com>
2011-02-18 16:45:12 -0500

    Adding context

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -2,29 +2,31 @@ require 'spec_helper'
 require 'lumberg/whm'
 
 describe Whm::Server do
-  before(:each) do
-    @login = { host: 'myhost.com', hash: 'iscool' }
-  end
+  context "Setting up the server" do
+    before(:each) do
+      @login = { host: 'myhost.com', hash: 'iscool' }
+    end
 
-  it "should setup host and hash" do
-    whm   = Whm::Server.new(@login)
-  
-    whm.host.should == @login[:host]
-    whm.hash.should == @login[:hash]
-  end
+    it "should setup host and hash" do
+      whm   = Whm::Server.new(@login)
+    
+      whm.host.should == @login[:host]
+      whm.hash.should == @login[:hash]
+    end
 
-  it "should transform the host into an SSL URL by default" do
-    whm = Whm::Server.new(@login)
-    whm.url.should == "https://#{@login[:host]}:2087"
-  end
+    it "should transform the host into an SSL URL by default" do
+      whm = Whm::Server.new(@login)
+      whm.url.should == "https://#{@login[:host]}:2087"
+    end
 
-  it "should transform the host into an SSL URL when asked" do
-    whm = Whm::Server.new(@login.merge(:ssl => true))
-    whm.url.should == "https://#{@login[:host]}:2087"
-  end
+    it "should transform the host into an SSL URL when asked" do
+      whm = Whm::Server.new(@login.merge(:ssl => true))
+      whm.url.should == "https://#{@login[:host]}:2087"
+    end
 
-  it "should transform the host into a non SSL URL" do
-    whm = Whm::Server.new(@login.merge(:ssl => false))
-    whm.url.should == "http://#{@login[:host]}:2086"
+    it "should transform the host into a non SSL URL" do
+      whm = Whm::Server.new(@login.merge(:ssl => false))
+      whm.url.should == "http://#{@login[:host]}:2086"
+    end
   end
 end
--- spec/whm_spec.rb
+++ spec/whm_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 require 'lumberg/whm'
 
 describe Whm do
-  it "should format a URL" do
-    Whm::format_url("example.com").should == "https://example.com:2087"
-  end
+  context "Formatting a URL" do
+    it "should be a URL" do
+      Whm::format_url("example.com").should == "https://example.com:2087"
+    end
 
-  it "should format an SSL URL" do
-    Whm::format_url("example.com", :ssl => false).should == "http://example.com:2086"
+    it "should be an SSL URL" do
+      Whm::format_url("example.com", :ssl => false).should == "http://example.com:2086"
+    end
   end
 end
