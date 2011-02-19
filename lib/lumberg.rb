4719055ce69f01f2edeb746b0bee54b60e416c0a
Justin Mazzi <hh@mailheist.com>
2011-02-19 14:35:49 -0500

    Doc

--- lib/lumberg.rb
+++ lib/lumberg.rb
@@ -1,12 +1,13 @@
 $:.unshift(File.dirname(__FILE__))
 
+# External Libs
 require 'json'
 require 'uri'
 
+# Internal Libs
 require 'lumberg/version'
 require 'lumberg/exceptions'
 require 'lumberg/whm'
-
 
 module Lumberg
 end
