6ed2c8ecd1022b71b380feeb863f106a5b97ef9b
Justin Mazzi <jmazzi@gmail.com>
2011-02-22 10:28:40 -0500

    Adding better suppport for live testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'vcr'
 VCR.config do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.stub_with :fakeweb
+  c.default_cassette_options = {record: :new_episodes}
 end
 
 def live_test?
