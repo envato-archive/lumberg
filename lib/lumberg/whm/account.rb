580cb488fefa7720aada80566149eba4ece273ab
Justin Mazzi <jmazzi@gmail.com>
2011-02-23 08:50:20 -0500

    Refacotored to do methods instead of assignments

--- lib/lumberg/whm/account.rb
+++ lib/lumberg/whm/account.rb
@@ -8,7 +8,7 @@ module Lumberg
 
       def initialize(options = {})
         Args.new(options) do |c|
-          c.requires = [:server]
+          c.requires :server
         end
      
         setup_server options.delete(:server) 
@@ -17,21 +17,21 @@ module Lumberg
       # WHM functions
       def create(options = {})
         Args.new(options) do |c|
-          c.requires  = [:username, :domain, :password]
-          c.booleans  = [:savepkg, :ip, :cgi, :frontpage, :hasshell, :useregns, :reseller, :forcedns]
-          c.optionals = [:plan, :pkgname, :savepkg, :featurelist, :quota, :password, :ip, :cgi, 
+          c.requires  :username, :domain, :password
+          c.booleans  :savepkg, :ip, :cgi, :frontpage, :hasshell, :useregns, :reseller, :forcedns
+          c.optionals :plan, :pkgname, :savepkg, :featurelist, :quota, :password, :ip, :cgi, 
                        :frontpage, :hasshell, :contactemail, :cpmod, :maxftp, :maxsql, :maxpop, :maxlst, 
                        :maxsub, :maxpark, :maxaddon, :bwlimit, :customip, :language, :useregns, :hasuseregns, 
-                       :reseller, :forcedns, :mxcheck, :username, :domain]
+                       :reseller, :forcedns, :mxcheck, :username, :domain
         end
         server.perform_request('createacct', options)
       end
 
       def remove(options = {})
         Args.new(options) do |c|
-          c.requires  = [:username]
-          c.optionals = [:username, :keepdns]
-          c.booleans  = [:keepdns]
+          c.requires  :username
+          c.optionals :username, :keepdns
+          c.booleans  :keepdns
         end
 
         options[:user] = options.delete(:username)
@@ -40,9 +40,9 @@ module Lumberg
 
       def change_password(options = {})
         Args.new(options) do |c|
-          c.requires  = [:username, :pass]
-          c.optionals = [:username, :pass, :db_pass_update]
-          c.booleans  = [:db_pass_update]
+          c.requires  :username, :pass
+          c.optionals :username, :pass, :db_pass_update
+          c.booleans  :db_pass_update
         end
 
         options[:user] = options.delete(:username)
