1b4e88c058aebebe7423fdc9d83b173dfca2fcf5
Justin Mazzi <jmazzi@gmail.com>
2011-03-07 16:50:56 -0500

    This stuff belongs in server

--- spec/whm/whm_spec.rb
+++ spec/whm/whm_spec.rb
@@ -3,40 +3,6 @@ require 'lumberg/whm'
 
 module Lumberg
   describe Whm do
-    before(:each) do
-      @host = 'example.com'
-    end
-
-    describe "#format_url" do
-      it "converts the host into an SSL URL by default" do
-        Whm::format_url(@host).should == "https://example.com:2087/json-api/"
-      end
-
-      it "converts the host into an SSL URL when asked" do
-        Whm::format_url(@host, :ssl => true).should == "https://example.com:2087/json-api/"
-      end
-
-      it "converts the host into a non SSL URL when asked" do
-        Whm::format_url(@host, :ssl => false).should == "http://example.com:2086/json-api/"
-      end
-    end
-
-    describe "#format_hash" do
-      it "raises an error if hash is not a string" do
-        expect{ Whm::format_hash(nil)}.to raise_error(Lumberg::WhmArgumentError, "Missing WHM hash")
-      end
-
-      it "removes \\n's from the hash" do
-        hash = Whm::format_hash("my\nhash\n")
-        hash.should == 'myhash'
-      end
-
-      it "removes whitespace from the hash" do
-        hash = Whm::format_hash("    my hash ")
-        hash.should == 'myhash'
-      end
-    end
-
     describe "#to_bool" do
       it "converts all 1/0 values to bools" do
         hash = {:true => 1, :false => 0, :other => 2}
