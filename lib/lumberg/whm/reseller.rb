fc4f3323546d9d44151dbbd173420c274ae5cc31
Justin Mazzi <jmazzi@gmail.com>
2011-03-09 13:18:00 -0500

    Adding Reseller#create

--- lib/lumberg/whm/reseller.rb
+++ lib/lumberg/whm/reseller.rb
@@ -1,6 +1,14 @@
 module Lumberg
   module Whm
     class Reseller < Base
+      def create(options = {})
+        Args.new(options) do |c| 
+          c.requires :username 
+          c.booleans :makeowner
+        end
+        options[:user] = options.delete(:username)
+        server.perform_request('setupreseller', options)
+      end
     end
   end
 end
--- spec/whm/reseller_spec.rb
+++ spec/whm/reseller_spec.rb
@@ -5,6 +5,25 @@ module Lumberg
     before(:each) do
       @login    = { :host => @whm_host, :hash => @whm_hash }
       @whm      = Whm::Server.new(@login.dup)
+      @reseller = Whm::Reseller.new(:server => @whm)
+    end
+
+    describe "#setup_reseller" do
+      use_vcr_cassette "whm/reseller/setupreseller"
+
+      it "requires username" do
+        requires_attr('username') { @reseller.create }
+      end
+
+      it "creates a reseller" do
+        result = @reseller.create(:username => 'bob')
+        result[:success].should be_true
+      end
+
+      it "accepts makeowner option" do
+        @reseller.server.should_receive(:perform_request).with('setupreseller', hash_including(:makeowner => true))
+        @reseller.create(:username => 'bob', :makeowner => true)
+      end
     end
   end
 end
