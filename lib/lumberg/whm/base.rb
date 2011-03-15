5cb3179df11091f0962bf3a8f52eda613c3afd0c
Justin Mazzi <jmazzi@gmail.com>
2011-03-15 09:04:51 -0400

    This belongs over here

--- lib/lumberg/whm/base.rb
+++ lib/lumberg/whm/base.rb
@@ -20,23 +20,6 @@ module Lumberg
         end
       end
 
-      # Creates WHM::Whatever.new(:server => @server)
-      # automagically
-      def auto_accessors
-        [:account, :dns, :reseller]
-      end
-
-      def method_missing(meth, *args, &block)
-        if auto_accessors.include?(meth.to_sym)
-          ivar = instance_variable_get("@#{meth}")
-          if ivar.nil?
-            constant  = Whm.const_get(meth.to_s.capitalize)
-            return instance_variable_set("@#{meth}", constant.new(:server => @server))
-          end
-        else
-          super
-        end
-      end
     end
   end
 end
