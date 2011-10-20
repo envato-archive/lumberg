ff46b19413df7d403d4d805bfd712f19c133b1c9
Dan Richert <dan.richert@gmail.com>
2011-10-20 17:38:57 -0400

    wip

--- spec/cpanel/base_spec.rb
+++ spec/cpanel/base_spec.rb
@@ -7,11 +7,14 @@ module Lumberg
     before(:each) do
       # TODO: DRY (also in whm/base_spec.rb)
       @login  = { :host => @whm_host, :hash => @whm_hash }
-      @server = Whm::Server.new(@login.dup)
-      @base   = Whm::Base.new(:server => @server)
+      @server = Whm::Server.new @login.dup
+      @base   = Cpanel::Base.new :server => @server
     end
 
     describe "#initialize" do
+      it "allows a server instance to be passed in" do
+
+      end
     end
   end
 end
