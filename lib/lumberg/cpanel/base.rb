09c947a60226469ee2579e7baad1ceb72597cbe5
Dan Richert <dan.richert@gmail.com>
2011-11-17 18:16:03 -0500

    Cpanel::Base w/ spec

--- lib/lumberg/cpanel/base.rb
+++ lib/lumberg/cpanel/base.rb
@@ -1,7 +1,16 @@
 module Lumberg
   module Cpanel
-    class Base << Whm::Base
+    class Base < Whm::Base
+      @@server = nil
+
       def initialize(options = {})
+        if options.empty? && !@@server.nil?
+          super :server => @@server
+        else
+          super options
+        end
+
+        @@server = @server
       end
     end
   end
--- spec/cpanel/base_spec.rb
+++ spec/cpanel/base_spec.rb
@@ -5,15 +5,48 @@ require 'lumberg/cpanel'
 module Lumberg
   describe Cpanel::Base do
     before(:each) do
-      # TODO: DRY (also in whm/base_spec.rb)
       @login  = { :host => @whm_host, :hash => @whm_hash }
-      @server = Whm::Server.new @login.dup
-      @base   = Cpanel::Base.new :server => @server
+      @server = Whm::Server.new(@login.dup)
+      @base   = Cpanel::Base.new(:server => @server)
     end
 
     describe "#initialize" do
-      it "allows a server instance to be passed in" do
+      context "a server instance or server hash is passed in" do
+        it "allows a server instance to be passed in" do
+          @base.server.should be_a(Whm::Server)
+        end
 
+        it "allows a server hash to be passed in" do
+          base = Cpanel::Base.new(:server => @login)
+          base.server.should be_a(Whm::Server)
+        end
+
+        it "caches the server instance" do
+          Cpanel::Base.class_variable_get(:@@server).should eql(@base.server)
+        end
+      end
+
+      context "nothing is passed in" do
+        context "a server instance is cached" do
+          it "loads the cached server instance" do
+            base = Cpanel::Base.new
+            base.server.should eql(
+              Cpanel::Base.class_variable_get(:@@server)
+            )
+          end
+        end
+
+        context "server instance is not cached" do
+          before { Cpanel::Base.class_variable_set(:@@server,nil) }
+
+          it "raises an error" do
+            expect {
+              Cpanel::Base.new
+            }.to raise_error(
+              WhmArgumentError, /Missing required param/
+            )
+          end
+        end
       end
     end
   end
